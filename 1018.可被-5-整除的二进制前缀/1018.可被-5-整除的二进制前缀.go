03475d495f7060ad906c154781cf23af1a99982b
lee <1340551309@qq.com>
2021-01-18 22:38:29 +0800

    20210118

--- 1018.可被-5-整除的二进制前缀/1018.可被-5-整除的二进制前缀.go
+++ 1018.可被-5-整除的二进制前缀/1018.可被-5-整除的二进制前缀.go
@@ -14,19 +14,27 @@
 
 func prefixesDivBy5(A []int) []bool {
 	ans := []bool{}
-	for i := 0 ; i < len(A); i++ {
-		num := 0
-		for j := 0 ; j <= i ; j++ {
-			num = num + A[j]*int(math.Pow(2,float64(i-j)))
-		}
-		// fmt.Println(num)
-		if num%5 == 0 {
-			ans = append(ans,true)
-			// fmt.Println("t")
-		}else{
-			ans = append(ans,false)
-			// fmt.Println("f")
-		}
+	// for i := 0 ; i < len(A); i++ {
+	// 	num := 0
+	// 	for j := 0 ; j <= i ; j++ {
+	// 		num = num + A[j]*int(math.Pow(2,float64(i-j)))
+	//		计算出完整值容易溢出，可能是这个原因造成了无法通过
+	// 	}
+	// 	// fmt.Println(num)
+	// 	if num%5 == 0 {
+	// 		ans = append(ans,true)
+	// 		// fmt.Println("t")
+	// 	}else{
+	// 		ans = append(ans,false)
+	// 		// fmt.Println("f")
+	// 	}
+	// }
+
+	//计算到第ｉ位时，除以5得到的余数
+	prefix := 0
+	for _, v := range A {
+		prefix = (prefix<<1 | v) % 5
+		ans = append(ans, prefix == 0)
 	}
 	return ans
 }
---
+++ 721.账户合并.go
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=721 lang=golang
+ *
+ * [721] 账户合并
+ */
+
+// @lc code=start
+// func accountsMerge(accounts [][]string) [][]string {
+// 	parent := map[string]int{}
+// 	account := map[int][]string{}
+// 	for i , v := range accounts {
+// 		nowParent := i
+// 		for j := 1 ; j < len(v) ; j++ {
+// 			if p,ok := parent[v[j]];ok{
+// 				if p < nowParent {
+// 					nowParent = p
+// 				}
+// 			} 
+// 		}
+// 		for j := 1 ; j < len(v) ; j++ {
+// 			if p,ok := parent[v[j]];!ok{
+// 				account[nowParent] = append(account[nowParent],v[j])
+// 				sort.Strings(account[nowParent])
+// 				parent[v[j]] = nowParent
+// 			}else{
+// 				if p != nowParent {
+// 					for _,vv := range account[p] {
+// 						account[nowParent] = append(account[nowParent],vv)
+// 						sort.Strings(account[nowParent])
+// 					}
+// 					delete(account,p)
+// 					parent[v[j]] = nowParent
+// 				}
+// 			}
+// 		}
+// 	}
+// 	ans := [][]string{}
+// 	for k,v := range account {
+// 		oneaccount := []string{}
+// 		oneaccount = append(oneaccount,accounts[k][0])
+// 		for _,vv := range v {
+// 			oneaccount = append(oneaccount,vv)
+// 		}
+// 		ans = append(ans,oneaccount)
+// 	}
+// 	return ans
+// }
+func accountsMerge(accounts [][]string) (ans [][]string) {
+    emailToIndex := map[string]int{}
+    emailToName := map[string]string{}
+    for _, account := range accounts {
+        name := account[0]
+        for _, email := range account[1:] {
+            if _, has := emailToIndex[email]; !has {
+                emailToIndex[email] = len(emailToIndex)
+                emailToName[email] = name
+            }
+        }
+    }
+
+    parent := make([]int, len(emailToIndex))
+    for i := range parent {
+        parent[i] = i
+    }
+    var find func(int) int
+    find = func(x int) int {
+        if parent[x] != x {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+    union := func(from, to int) {
+        parent[find(from)] = find(to)
+    }
+
+    for _, account := range accounts {
+        firstIndex := emailToIndex[account[1]]
+        for _, email := range account[2:] {
+            union(emailToIndex[email], firstIndex)
+        }
+    }
+
+    indexToEmails := map[int][]string{}
+    for email, index := range emailToIndex {
+        index = find(index)
+        indexToEmails[index] = append(indexToEmails[index], email)
+    }
+
+    for _, emails := range indexToEmails {
+        sort.Strings(emails)
+        account := append([]string{emailToName[emails[0]]}, emails...)
+        ans = append(ans, account)
+    }
+    return
+}
+// @lc code=end
+
