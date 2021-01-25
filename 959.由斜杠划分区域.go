04b2c34c3ad0ef338dd9df860fdbf976b5941e01
lee <1340551309@qq.com>
2021-01-25 20:51:16 +0800

    20210125,959，cv的，但看懂了，还得多积累

--- 959.由斜杠划分区域.go
+++ 959.由斜杠划分区域.go
@@ -44,32 +44,32 @@ func (uf *unionFind) union(x, y int) {
 
 func regionsBySlashes(grid []string) int {
     n := len(grid)//方形边长，n*n
-    uf := newUnionFind(4 * n * n)
+    uf := newUnionFind(4 * n * n)//注意哟，n*n个小方块，每个小方块分成四片
     for i := 0; i < n; i++ {
         for j := 0; j < n; j++ {
-            idx := i*n + j
-            if i < n-1 {
+            idx := i*n + j	//标记第几个消防快
+            if i < n-1 {//不是最底下一列
                 bottom := idx + n
-                uf.union(idx*4+2, bottom*4)
+                uf.union(idx*4+2, bottom*4)//每一小方块，从上然后顺时针编号为0~3,2为下，0(4)为上
             }
-            if j < n-1 {
+            if j < n-1 {//不是最右边一列
                 right := idx + 1
-                uf.union(idx*4+1, right*4+3)
+                uf.union(idx*4+1, right*4+3)//１为右，3为左
             }
             if grid[i][j] == '/' {
-                uf.union(idx*4, idx*4+3)
-                uf.union(idx*4+1, idx*4+2)
+                uf.union(idx*4, idx*4+3)//上左
+                uf.union(idx*4+1, idx*4+2)//右下
             } else if grid[i][j] == '\\' {
-                uf.union(idx*4, idx*4+1)
-                uf.union(idx*4+2, idx*4+3)
+                uf.union(idx*4, idx*4+1)//上右
+                uf.union(idx*4+2, idx*4+3)//下左
             } else {
                 uf.union(idx*4, idx*4+1)
                 uf.union(idx*4+1, idx*4+2)
                 uf.union(idx*4+2, idx*4+3)
-            }
+            }//秒啊，太秒了
         }
     }
-    return uf.setCount
+    return uf.setCount//初始为4*n*n最终返回剩余总的连通区域个数
 }
 // @lc code=end
 
