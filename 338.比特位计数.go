f66c69ccf6b47fadd70494d5d0b126b61df00af2
handongyexingren <1340551309@qq.com>
2021-03-03 10:16:11 +0800

    有提升

--- 338.比特位计数.go
+++ 338.比特位计数.go
@@ -6,7 +6,7 @@
 
 // @lc code=start
 func countBits(num int) []int {
-	
+	/*
 	ans := make([]int,num+1)
 	back := 1
 	for i,j := 1,back ; i <= num ; i++ {
@@ -18,7 +18,24 @@ func countBits(num int) []int {
 		}
 	}
 	return ans
-	
+	*/
+
+	/*
+	index 0 1 2 3 4 5 6 7 8
+	value 0 1 1 2 1 2 2 3 1
+	初始0，每位加1复制一份添加到末尾 0 1
+	每位加1复制一份添加到末尾 0 1 1 2
+	每位加1复制一份添加到末尾 0 1 1 2 1 2 2 3
+	*/
+	ans := make([]int,num+1)
+	for i,start := 1,1 ; i <= num ; i++ {
+		if i/2 == start {
+			start *= 2
+		}
+		ans[i] = ans[i-start]+1
+	}
+	return ans
+
 	/*
 	ans := make([]int,num+1)
 	if num == 0 {
