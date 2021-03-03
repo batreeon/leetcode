93361682420077ccf6e352965f341305c4741cc5
handongyexingren <1340551309@qq.com>
2021-03-03 10:00:26 +0800

    去掉了一些多余的，有提升

--- 338.比特位计数.go
+++ 338.比特位计数.go
@@ -6,13 +6,11 @@
 
 // @lc code=start
 func countBits(num int) []int {
-	ans := []int{0}
-	if num == 0 {
-		return ans
-	}
+	
+	ans := make([]int,num+1)
 	back := 1
 	for i,j := 1,back ; i <= num ; i++ {
-		ans = append(ans,ans[i-back]+1)
+		ans[i] = ans[i-back]+1
 		j--
 		if j == 0 {
 			back *= 2
@@ -20,6 +18,20 @@ func countBits(num int) []int {
 		}
 	}
 	return ans
+	
+	/*
+	ans := make([]int,num+1)
+	if num == 0 {
+		return ans
+	}
+	back := 1
+	for i:= 1; i <= num ; i *= 2 {
+		// ans[i:] = ans[i-back:i]+1
+		//想法就是直接把前back个数据，再复制一份，并每位+1，但怎样实现呢？
+		back *=2
+	}
+	return ans
+	*/
 }
 // @lc code=end
 
