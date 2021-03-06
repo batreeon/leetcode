e5871df01fb2e1310b029370ee00ee5507d2f4d6
handongyexingren <1340551309@qq.com>
2021-03-06 16:59:28 +0800

    没做完

---
+++ 347/347.前-k-个高频元素.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=golang
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+package main
+func topKFrequent(nums []int, k int) []int {
+	//记录每个数的重复次数
+	m := map[int]int{}
+	for _,n := range nums {
+		m[n]++
+	}
+	
+	times := [][]int{}
+	for k,v := range m {
+		times = append(times,[]int{k,v})
+	}
+
+	partition := func(times [][]int,l,r int) int {
+		x := times[r][1]
+		i := l - 1
+		for j := l ; j < r ; j++ {
+			if times[j][1] >= x {//大的排前面
+				i++
+				times[i],times[j] = times[j],times[i]
+			}
+		}
+		times[i+1],times[r] = times[r],times[i+1]
+		return i+1
+	}
+
+	start,end := 0,len(times)-1
+	p := partition(times,start,end)
+	for p < k-1 {
+		start = p+1
+		p = partition(times,start,end)
+	}
+	ans := []int{}
+	for i := 0 ; i < k ; i++ {
+		ans = append(ans,times[i][0])
+	}
+	return ans
+}
+// @lc code=end
+
---
+++ 347/347.前-k-个高频元素_test.go
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=golang
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_topKFrequent(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		// {
+		// 	"1",
+		// 	args{
+		// 		[]int{1,1,1,2,2,3},
+		// 		2,
+		// 	},
+		// 	[]int{1,2},
+		// },
+		{
+			"2",
+			args{
+				[]int{5,1,-1,-8,-7,8,-5,0,1,10,8,0,-4,3,-1,-1,4,-5,4,-3,0,2,2,2,4,-2,-4,8,-7,-7,2,-8,0,-8,10,8,-8,-2,-9,4,-7,6,6,-1,4,2,8,-3,5,-9,-3,6,-8,-5,5,10,2,-5,-1,-5,1,-3,7,0,8,-2,-3,-1,-5,4,7,-9,0,2,10,4,4,-4,-1,-1,6,-8,-9,-1,9,-9,3,5,1,6,-1,-2,4,2,4,-6,4,4,5,-5},
+				7,
+			},
+			[]int{4,-1,2,-5,-8,0,8},
+		},
+		
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := topKFrequent(tt.args.nums, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("topKFrequent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
