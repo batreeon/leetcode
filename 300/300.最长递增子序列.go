4b0cc48e6998b2e5d96d507290d59b9bafa936dd
handongyexingren <1340551309@qq.com>
2021-03-21 19:03:44 +0800

    复习了下300，354，300题学了下原来没看懂的贪心加二分查找，思想很好，不难理解

--- 300/300.最长递增子序列.go
+++ 300/300.最长递增子序列.go
@@ -7,7 +7,8 @@
 // @lc code=start
 package main
 func lengthOfLIS(nums []int) int {
-	//分治
+	//动规
+	/*
 	l := len(nums)
 	f := make([]int,l)
 	max := func(x,y int) int {
@@ -26,6 +27,43 @@ func lengthOfLIS(nums []int) int {
 		}
 		f[i] = maxf + 1
 		ans = max(ans,f[i])
+	}
+	//f[i] = max(f[j])+1,j < i 并且 nums[j]<nums[j]
+	return ans
+	*/
+
+	//贪心加二分查找
+	//维护一个递增序列，使这个递增序列的最大值，是同长度递增序列中最小的，这就是贪心体现的地方
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	ans := 1
+	d := make([]int,0,l)
+	d = append(d,nums[0])
+	for _,v := range nums {
+		if v > d[ans-1] {
+			// 递增序列变长了，直接把这个新的更大的元素加到末尾
+			d = append(d,v)
+			ans++
+		}else{
+			// 为了贪心，递增序列中的元素每次尽可能增得慢一点
+			// 用较小的元素替换已有的递增序列
+			pos := -1
+			l,r := 0,ans-1
+
+			// 二分查找找到从后往前数第一个比当前元素小的元素，然后插到他后面，替换原来的较大的递增值
+			for l <= r {
+				mid := (l+r)>>1
+				if d[mid] < v {
+					pos = mid
+					l = mid+1
+				}else{
+					r = mid-1
+				}
+			}
+			d[pos+1] = v
+		}
 	}
 	return ans
 }
--- 354/354.俄罗斯套娃信封问题.go
+++ 354/354.俄罗斯套娃信封问题.go
@@ -6,12 +6,15 @@
 
 // @lc code=start
 package main
+
 import "sort"
-type doubleSort [][]int 
+
+type doubleSort [][]int
+
 func (ds doubleSort) Len() int {
 	return len(ds)
 }
-func (ds doubleSort) Less(i,j int) bool {
+func (ds doubleSort) Less(i, j int) bool {
 	// if ds[i][0] < ds[j][0] && ds[i][1] < ds[j][1] {
 	// 	return true
 	// }
@@ -27,8 +30,8 @@ func (ds doubleSort) Less(i,j int) bool {
 	}
 	return false
 }
-func (ds doubleSort) Swap(i,j int) {
-	ds[i],ds[j] = ds[j],ds[i]
+func (ds doubleSort) Swap(i, j int) {
+	ds[i], ds[j] = ds[j], ds[i]
 }
 
 func maxEnvelopes(envelopes [][]int) int {
@@ -39,56 +42,58 @@ func maxEnvelopes(envelopes [][]int) int {
 	sort.Sort(ds)
 
 	/*
-	triangle := [][][]int{}
-	f := [][]int{}//标记路径
-	start := 0
-	for i := 1 ; i < len(ds) ; i++ {
-		if ds.Less(i-1,i) {
-			triangle = append(triangle,ds[start:i])
-			f = append(f,make([]int,i-start))
-			start = i
-		}
-	}
-	if start < len(ds) {
-		triangle = append(triangle,ds[start:])
-		f = append(f,make([]int,len(ds)-start))
-	}
-	if len(triangle) == 1 {
-		return 1
-	}
-	//ans := 0
-	l := len(triangle)
-	for i := 0 ; i < l ; i++ {
-		for j := 0 ; j < len(triangle[i]) ; j++ {
-			if i == 0 {
-				f[i][j] = 1
-			}else{
-
+		triangle := [][][]int{}
+		f := [][]int{}//标记路径
+		start := 0
+		for i := 1 ; i < len(ds) ; i++ {
+			if ds.Less(i-1,i) {
+				triangle = append(triangle,ds[start:i])
+				f = append(f,make([]int,i-start))
+				start = i
 			}
 		}
-	}
-	return len(triangle)
+		if start < len(ds) {
+			triangle = append(triangle,ds[start:])
+			f = append(f,make([]int,len(ds)-start))
+		}
+		if len(triangle) == 1 {
+			return 1
+		}
+		//ans := 0
+		l := len(triangle)
+		for i := 0 ; i < l ; i++ {
+			for j := 0 ; j < len(triangle[i]) ; j++ {
+				if i == 0 {
+					f[i][j] = 1
+				}else{
+
+				}
+			}
+		}
+		return len(triangle)
 	*/
-	max := func(x,y int) int {
+	max := func(x, y int) int {
 		if x > y {
 			return x
 		}
 		return y
 	}
 	l := len(ds)
-	f := make([]int,l)
+	f := make([]int, l)
 	ans := 0
-	for i := 0 ; i < l ; i++ {
+	// 本质和300题一样，找最长严格递增序列
+	// 前面定义的doublesort排序规则很重要，先按长递增排序，若长相同，则按宽递减排序
+	for i := 0; i < l; i++ {
 		maxf := 0
-		for j := 0 ; j < i ; j++ {
+		for j := 0; j < i; j++ {
 			if ds[j][1] < ds[i][1] {
-				maxf = max(maxf,f[j])
+				maxf = max(maxf, f[j])
 			}
 		}
 		f[i] = maxf + 1
-		ans = max(ans,f[i])
+		ans = max(ans, f[i])
 	}
 	return ans
 }
-// @lc code=end
 
+// @lc code=end
