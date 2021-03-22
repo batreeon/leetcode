b29f93d5e96db02dc66c9d86328ad8edf6f9216b
handongyexingren <1340551309@qq.com>
2021-03-22 21:14:43 +0800

    简单题

---
+++ 191.位-1-的个数.go
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=191 lang=golang
+ *
+ * [191] 位1的个数
+ */
+
+// @lc code=start
+func hammingWeight(num uint32) int {
+    ans := 0
+	for num != 0 {
+		ans++
+		num &= num-1
+	}
+	return ans
+}
+// @lc code=end
+
