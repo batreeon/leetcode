044d9f8612c573885953e8b022de750aed383c8d
handongyexingren <1340551309@qq.com>
2021-03-23 16:43:10 +0800

    排列组合

---
+++ 357.计算各个位数不同的数字个数.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=357 lang=golang
+ *
+ * [357] 计算各个位数不同的数字个数
+ */
+
+// @lc code=start
+func countNumbersWithUniqueDigits(n int) int {
+	/*
+	// 多放了几个现成解
+	ans := []int{1,10,91,739,5275}
+	if n <= 4 {
+		return ans[n]
+	}
+	for i := 5 ; i <= n ; i++ {
+		temp := 1
+		for j := 9 ; j > 9-i+1 ; j-- {
+			temp *= j
+		}
+		ans = append(ans,ans[i-1]+temp*9)
+	}
+	return ans[n]
+	*/
+
+	// 提升？没有提升！
+	ans := 1
+	if n > 0 {
+		for i := 1 ; i <= n ; i++ {
+			temp := 1
+			for j := 9 ; j > 9-i+1 ; j-- {
+				temp *= j
+			}
+			ans += temp*9
+		}
+	}
+	return ans
+}
+// @lc code=end
+
