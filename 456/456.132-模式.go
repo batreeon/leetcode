c22f68584857781e5d473f5eeb810596dc5296d1
handongyexingren <1340551309@qq.com>
2021-03-24 09:45:46 +0800

    20210324，做过又忘了，做的有点暴力

--- 456/456.132-模式.go
+++ 456/456.132-模式.go
@@ -8,13 +8,14 @@
 package main
 import "math"
 
-
 func find132pattern(nums []int) bool {
+	// 逻辑：从下下标1到n-1遍历，这是指示中间元素。
+	// 找他左侧最小的元素，找他右侧比他小的最大的元素，如果符合132模式，就返回true
+	// 若为false,就继续遍历
 	l := len(nums)
 	if l < 3 {
 		return false
 	}
-
 
 	min := func(x,y int) int {
 		if x < y {
@@ -46,10 +47,12 @@ func find132pattern(nums []int) bool {
 			continue
 		}
 		
+		// 第一次执行，即rightMax == math.MaxInt64时
+		// nums[i-1]>=nums[i]是为了判断，rightMax是否变化，没有变化，就不需要再调用findMaxbelowThreshold函数了
 		if nums[i-1] >= nums[i] || rightMax != math.MaxInt64 {
 			rightMax = findMaxbelowThreshold(nums,nums[i],i+1,l-1)
 		}
-		
+
 		if nums[i] > rightMax && rightMax > leftMin {
 			return true
 		}
