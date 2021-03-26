1cf009691f4d2e691e7af0f1f5a9c21420dba8d6
handongyexingren <1340551309@qq.com>
2021-03-26 12:36:59 +0800

    姊妹题，但是II因为有重复元素而带来了一些问题，在判断有序时，不能简单比较两端元素

--- 33/33.搜索旋转排序数组_test.go
+++ 33/33.搜索旋转排序数组_test.go
@@ -3,8 +3,10 @@ import "testing"
 func TestSearch(t *testing.T) {
 	// got := search([]int{4,5,6,7,0,1,2},0)
 	// want := 4
-	got := search([]int{5,1,3},0)
-	want := -1
+	// got := search([]int{5,1,3},0)
+	// want := -1
+	got := search([]int{5,0,1,2,4},0)
+	want := 1
 	if got != want {
 		t.Error("got",got,"want",want)
 	}
---
+++ 81.搜索旋转排序数组-ii.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=81 lang=golang
+ *
+ * [81] 搜索旋转排序数组 II
+ */
+
+// @lc code=start
+package main
+import "sort"
+
+func search(nums []int, target int) bool {
+	n := len(nums)
+	if n == 0 {
+		return false
+	}
+	if n == 1 {
+		if nums[0] == target {
+			return true
+		}
+		return false
+	}
+	l,r := 0,n-1
+	for l <= r {
+		mid := (l+r)/2
+		if nums[mid] == target {
+			return true
+		}
+		// 1,0,1,1,1,不能直接用nums[l] <= nums[mid]是否有序，
+		// 因为当l==0,mid==2时，即1,0,1序列，他也判定为有序
+		// 这是有重复元素带来的问题
+		if sort.IsSorted(sort.IntSlice(nums[l:mid+1])) {
+			if nums[l] <= target && target < nums[mid]{
+				r = mid-1
+			}else{
+				l = mid+1
+			}
+		}else{
+			if nums[mid] < target && target <= nums[r] {
+				l = mid+1
+			}else{
+				r = mid-1
+			}
+		}
+	}
+	return false
+}
+// @lc code=end
+
