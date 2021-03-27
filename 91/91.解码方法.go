6a3073bc3c11d94460e535c401650ca9d98e95ba
handongyexingren <1340551309@qq.com>
2021-03-27 15:34:30 +0800

    动态规划，自底向上，从右到左，不断分割

---
+++ 91/91.解码方法.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=91 lang=golang
+ *
+ * [91] 解码方法
+ */
+
+// @lc code=start
+package main
+import "strconv"
+
+func numDecodings(s string) int {
+	// 0没有对应的，但是10，20有对应的
+	l := len(s)
+	if l == 0 {
+		return 0
+	}
+	if s[l-1] == '0' && l == 1{
+		return 0
+	}
+	if s[l-1] == '0' && (s[l-2] != '1' && s[l-2] != '2') {
+		return 0
+	}
+	dp := make([]int,l+1)
+	dp[l] = 1
+	for i := l-1 ; i >= 0 ; i-- {
+		if s[i] == '0' {
+			continue
+		}
+		res := dp[i+1]
+		for j := i+1 ; j < l ; j++ {
+			num,_ := strconv.Atoi(s[i:j+1])
+			if num >= 0 && num <= 26 {
+				res += dp[j+1]
+			}
+		}
+		dp[i] = res
+	}
+	return dp[0]
+}
+// @lc code=end
+
---
+++ 91/91.解码方法_test.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=91 lang=golang
+ *
+ * [91] 解码方法
+ */
+
+// @lc code=start
+
+package main
+
+import "testing"
+
+func Test_numDecodings(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			"1",
+			args{
+				string("10"),
+			},
+			1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numDecodings(tt.args.s); got != tt.want {
+				t.Errorf("numDecodings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
