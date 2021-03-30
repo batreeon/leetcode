416101dc5674cc4005a0bb8b52c90c666aafc447
handongyexingren <1340551309@qq.com>
2021-03-30 19:53:54 +0800

    删减掉了一些多余的操作，shame,时间复杂度提升了，空间提升不大

--- 143.重排链表.go
+++ 143.重排链表.go
@@ -13,6 +13,7 @@
  * }
  */
 func reorderList(head *ListNode)  {
+	/*
 	array1 := []*ListNode{}
 	for p := head ; p != nil ; {
 		array1 = append(array1,p)
@@ -30,6 +31,24 @@ func reorderList(head *ListNode)  {
 	pNode := head
 	for i := 1 ; i < n ; i++ {
 		pNode.Next = array2[i]
+		pNode = pNode.Next
+	}
+	pNode.Next = nil
+	*/
+
+	array := []*ListNode{}
+	for p := head ; p != nil ; {
+		array = append(array,p)
+		p = p.Next
+	}
+	n := len(array)
+	pNode := head
+	for i := 1 ; i < n ; i++ {
+		if i&1 == 1 {
+			pNode.Next = array[n-1-i/2]
+		}else{
+			pNode.Next = array[i/2]
+		}
 		pNode = pNode.Next
 	}
 	pNode.Next = nil
