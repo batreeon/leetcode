c94187cade7b0cc3a6508943e2b8956762f940ee
handongyexingren <1340551309@qq.com>
2021-03-30 23:53:32 +0800

    嵌套结构存储，不用存真实的字符，学到了学到了

--- 208.实现-trie-前缀树.go
+++ 208.实现-trie-前缀树.go
@@ -5,31 +5,84 @@
  */
 
 // @lc code=start
-package main
-import "strings"
+// package main
+// import "strings"
+
+// 时间慢，但空间还好
+// type Trie struct {
+// 	m map[string]bool
+// }
 
 
+// /** Initialize your data structure here. */
+// func Constructor() Trie {
+// 	m := map[string]bool{}
+// 	return Trie{m:m}
+// }
+
+
+// /** Inserts a word into the trie. */
+// func (this *Trie) Insert(word string)  {
+// 	this.m[word] = true
+// }
+
+
+// /** Returns if the word is in the trie. */
+// func (this *Trie) Search(word string) bool {
+// 	if _,ok := this.m[word] ; ok {
+// 		return true
+// 	}
+// 	return false
+// }
+
+
+// /** Returns if there is any word in the trie that starts with the given prefix. */
+// func (this *Trie) StartsWith(prefix string) bool {
+// 	for key,_ := range this.m {
+// 		if strings.HasPrefix(key,prefix) {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// 空间换时间
 type Trie struct {
-	m map[string]bool
+	child []*Trie
+	isWord bool
 }
 
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
-	m := map[string]bool{}
-	return Trie{m:m}
+	child := make([]*Trie,26)
+	return Trie{child:child}
 }
 
 
 /** Inserts a word into the trie. */
 func (this *Trie) Insert(word string)  {
-	this.m[word] = true
+	p := this
+	for _,w := range word {
+		if p.child[int(w-'a')] == nil {
+			p.child[int(w-'a')] = &Trie{child:make([]*Trie,26)}
+		}
+		p = p.child[int(w-'a')]
+	}
+	p.isWord = true
 }
 
 
 /** Returns if the word is in the trie. */
 func (this *Trie) Search(word string) bool {
-	if _,ok := this.m[word] ; ok {
+	p := this
+	for _,w := range word {
+		if p.child[int(w-'a')] == nil {
+			return false
+		}
+		p = p.child[int(w-'a')]
+	}
+	if p.isWord {
 		return true
 	}
 	return false
@@ -38,14 +91,15 @@ func (this *Trie) Search(word string) bool {
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
 func (this *Trie) StartsWith(prefix string) bool {
-	for key,_ := range this.m {
-		if strings.HasPrefix(key,prefix) {
-			return true
+	p := this
+	for _,w := range prefix {
+		if p.child[int(w-'a')] == nil {
+			return false
 		}
+		p = p.child[int(w-'a')]
 	}
-	return false
+	return true
 }
-
 
 /**
  * Your Trie object will be instantiated and called as such:
