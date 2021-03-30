f7513125b6312b4768586218b0a9dd10bd1e6671
handongyexingren <1340551309@qq.com>
2021-03-30 19:24:37 +0800

    深拷贝链表，先在源节点后复制一个，然后遍历处理random,然后把新节点提出来，注意不能改变原链表的结构

---
+++ 138.复制带随机指针的链表.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=golang
+ *
+ * [138] 复制带随机指针的链表
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+func copyRandomList(head *Node) *Node {
+	if head == nil {
+		return head
+	}
+	for pNode := head ; pNode != nil ; {
+		newNode := &Node{Val:pNode.Val,Next:pNode.Next}
+		pNode.Next = newNode
+		pNode = newNode.Next
+	}
+	for pNode := head ; pNode != nil ; {
+		if pNode.Random != nil {
+			pNode.Next.Random = pNode.Random.Next
+		}else{
+			pNode.Next.Random = pNode.Random
+		}
+		pNode = pNode.Next.Next
+	}
+	hhead := head.Next
+	for pNode := head ; pNode != nil ; {
+		next := pNode.Next.Next
+		if next == nil {
+			pNode.Next = nil
+			break
+		}
+		pNode.Next.Next = next.Next
+		pNode.Next = next
+		pNode = next
+	}
+	return hhead
+}
+// @lc code=end
+
