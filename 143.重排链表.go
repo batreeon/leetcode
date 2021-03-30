af45279c70d9945a9c04ce7ff49920606515aceb
handongyexingren <1340551309@qq.com>
2021-03-30 19:48:51 +0800

    啊啊啊，笨拙的暴力法，甚至让人多看一眼就觉得恶心的代码，超过百分之五，微笑

---
+++ 143.重排链表.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=143 lang=golang
+ *
+ * [143] 重排链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reorderList(head *ListNode)  {
+	array1 := []*ListNode{}
+	for p := head ; p != nil ; {
+		array1 = append(array1,p)
+		p = p.Next
+	}
+	n := len(array1)
+	array2 := make([]*ListNode,n)
+	for i := 1 ; i < n ; i++ {
+		if i&1 == 1 {
+			array2[i] = array1[n-1-i/2]
+		}else{
+			array2[i] = array1[i/2]
+		}
+	}
+	pNode := head
+	for i := 1 ; i < n ; i++ {
+		pNode.Next = array2[i]
+		pNode = pNode.Next
+	}
+	pNode.Next = nil
+}
+// @lc code=end
+
