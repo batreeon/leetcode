efd1b83bde15b9cfef2abc7fba58f0d969c33479
handongyexingren <1340551309@qq.com>
2021-03-31 08:52:47 +0800

    修正了我在回溯中的一些思维漏洞，注意组合和排列问题中的一些差异

--- 90.子集-ii.go
+++ 90.子集-ii.go
@@ -5,13 +5,23 @@
  */
 
 // @lc code=start
+package main
+import "sort"
+
 func subsetsWithDup(nums []int) [][]int {
+	
+	/*
+	// 以前通过的版本
 	var backTrack func(nums []int , pos int , res []int , result *[][]int)
 	backTrack = func(nums []int , pos int , res []int , result *[][]int) {
 		ans := make([]int,len(res))
 		copy(ans,res)
 		*result = append(*result,ans)
 		for i := pos ; i < len(nums) ; i++ {
+			// 之前自己写的现在却看不懂了，为什么判断重复不需用map呢
+			// 哦哦哦，我懂了，这个题是求组合，因此每次遍历的下标都是从上一个字符的后面开始的
+			// 那么如果遍历到的某一个元素在一串相同的数字中但不是第一个，那么在这一级中，这一相同的数字就已经被访问过了
+			// 况且，即使用了map,!m[i-1]是始终为true的，因为前面的元素被访问过又丢弃了
 			if i != pos && nums[i] == nums[i-1] {//防止重复
 				continue
 			}
@@ -27,6 +37,30 @@ func subsetsWithDup(nums []int) [][]int {
 	result := [][]int{}
 	res := []int{}
 	backTrack(sortedNums,0,res,&result)
+	return result
+	*/
+	
+
+	//20210331,每日一题
+	result := [][]int{}
+	n := len(nums)
+	var backtrack func(res []int,start int)
+	backtrack = func(res []int,start int) {
+		resCopy := make([]int,len(res))
+		copy(resCopy,res)
+		result = append(result,resCopy)
+		for i := start ; i < n ; i++ {
+			if i != start && nums[i] == nums[i-1] {
+				continue
+			}
+			res = append(res,nums[i])
+			backtrack(res,i+1)
+			res = res[:len(res)-1]
+		}
+	}
+	
+	sort.Ints(nums)
+	backtrack([]int{},0)
 	return result
 }
 // @lc code=end
