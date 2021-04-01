5850e2c57a8c8860d97453809a943ae7733844f0
handongyexingren <1340551309@qq.com>
2021-04-01 23:07:06 +0800

    之前有错，居然没改

--- stack.go
+++ stack.go
@@ -1,21 +1,21 @@
 type stack struct {
-	s []int
-	l int
+	nums []int
+	l    int
 }
 
 func (st *stack) push(x int) {
-	st.s = append(st.s,x)
+	st.nums = append(st.nums, x)
 	st.l++
 }
 
 func (st *stack) top() int {
-	return st.s[st.l-1]
+	return st.nums[st.l-1]
 	st.l--
 }
 
 func (st *stack) pop() {
-	st.s = st.s[:st.l-1]
+	st.nums = st.nums[:st.l-1]
 }
-func(st stack) isEmpty() bool {
+func (st stack) isEmpty() bool {
 	return st.l == 0
 }
