37a24583f31bd9b041575d91cf6c1cd50ab16647
handongyexingren <1340551309@qq.com>
2021-04-09 18:55:22 +0800

    80,1209,442,217,重复元素问题,复习一下.尤其是1209感觉自己思路还是很不错的,但容易忘了

--- 80.删除排序数组中的重复项-ii.go
+++ 80.删除排序数组中的重复项-ii.go
@@ -8,6 +8,7 @@
 func removeDuplicates(nums []int) int {
 	l := len(nums)
 	for i := 0 ; i < l ; i++ {
+		// 先判断是否有重复元素
 		if i != 0 && nums[i] == nums[i-1] {
 			// i指示第二个重复元素
 			j := i+1
@@ -16,6 +17,7 @@ func removeDuplicates(nums []int) int {
 			// 后面的元素全部前移，覆盖掉多余的元素
 			copy(nums[i+1:],nums[j:l])
 			l -= j-i-1
+			// i始终维护着已处理好,无多余元素的序列的末尾
 		}
 	}
 	// 有一个问题，后面的元素，会因为前面的元素迁移而跟着多次迁移，有点浪费
