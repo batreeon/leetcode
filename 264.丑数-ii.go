5dd002b34230c22b60e68c901b5bc0f4bb27687d
handongyexingren <1340551309@qq.com>
2021-04-11 12:27:03 +0800

    丑数问题基本就大同小异，每次将下一个最小值加入进去就可以了

---
+++ 264.丑数-ii.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=264 lang=golang
+ *
+ * [264] 丑数 II
+ */
+
+// @lc code=start
+func nthUglyNumber(n int) int {
+	res := []int{1}
+	index2,index3,index5 := 0,0,0
+	min := func(x,y int) int {
+		if x < y {
+			return x
+		}
+		return y
+	}
+	
+	for {
+		if len(res) == n {
+			return res[n-1]
+		}
+		next := min(2*res[index2],min(3*res[index3],5*res[index5]))
+		res = append(res,next)
+		for 2*res[index2] <= next {
+			index2++
+		}
+		for 3*res[index3] <= next {
+			index3++
+		}
+		for 5*res[index5] <= next {
+			index5++
+		}
+	}
+}
+// @lc code=end
+
