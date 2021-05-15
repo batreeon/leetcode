f4550cf252d5cea8a365a714ada2bc47e5f7b093
handongyexingren <1340551309@qq.com>
2021-05-15 16:54:31 +0800

    我是不是做复杂了，20210515

---
+++ 13.罗马数字转整数.go
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=13 lang=golang
+ *
+ * [13] 罗马数字转整数
+ */
+
+// @lc code=start
+func romanToInt(s string) int {
+	ss := []byte(s)
+	m := map[string]int{}
+	m["I"] = 1
+	m["IV"] = 4
+	m["V"] = 5
+	m["IX"] = 9
+	m["X"] = 10
+	m["XL"] = 40
+	m["L"] = 50
+	m["XC"] = 90
+	m["C"] = 100
+	m["CD"] = 400
+	m["D"] = 500
+	m["CM"] = 900
+	m["M"] = 1000
+	result := 0
+	for len(ss) > 0 {
+		if len(ss) >= 2 {
+			if v,ok := m[string(ss[len(ss)-2:])] ; ok {
+				result += v
+				ss = ss[:len(ss)-2]
+			}else{
+				result += m[string(ss[len(ss)-1:])]
+				ss = ss[:len(ss)-1]
+			}
+		}else{
+			result += m[string(ss[len(ss)-1:])]
+			ss = ss[:len(ss)-1]
+		}
+	}
+	return result
+}
+// @lc code=end
+
