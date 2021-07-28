7a7e867667fbefe614905667f4a53ceade66606d
handongyexingren <1340551309@qq.com>
2021-07-28 09:14:33 +0800

    剪绳子问题，尽量分成3，能分成3就分成3，末尾剩0,1,2，分别相当于3,0 ; 3,1;  3,2;  3,1那一组可以重组为2，2因为2*2 > 3*1

---
+++ rngoffer/剑指 Offer 14- II. 剪绳子 II.go
@@ -0,0 +1,16 @@
+package main
+
+import "math"
+
+func cuttingRope(n int) int {
+	mod := int(1e9+7)
+	if n <= 3 {
+		return n-1
+	}
+	result := 1
+	for n > 4 {
+		result = result*3%mod
+		n -= 3
+	}
+	return result*n%mod
+}
