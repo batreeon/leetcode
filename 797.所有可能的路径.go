10736aa1d29c1e61e29d89b87d9df12039b1bf1c
handongyexingren <1340551309@qq.com>
2021-08-25 17:03:04 +0800

    20210825,mid,有向图，可以用dfs，但我写的这个第二种递归也挺有意思

--- 797.所有可能的路径.go
+++ 797.所有可能的路径.go
@@ -9,14 +9,13 @@ func allPathsSourceTarget(graph [][]int) [][]int {
 
 	/*
 	n := len(graph)
-	// 当前遍历路径下是否已经走过了
-	seen := make([]bool,n)
 	// 记录经该点是否能到终点,不能到就记录下来,以后遇到就直接不走了
-	// 但加了这个成绩没差别
-	available := map[int]bool{}
+	// 但加了这个成绩没大差别
+	unavailable := map[int]bool{}
 	// 保存可行路径
 	result := [][]int{}
 
+	// 其实就是dfs吧
 	var backtrack func(res []int) bool
 	backtrack = func(res []int) bool {
 		now := res[len(res)-1]
@@ -24,24 +23,22 @@ func allPathsSourceTarget(graph [][]int) [][]int {
 			resCopy := make([]int,len(res))
 			copy(resCopy,res)
 			result = append(result,resCopy)
+			// 只有最后到达n-1了才会返回true
 			return true
 		}
 		flag := false
 		for _,next := range graph[now] {
-			if !seen[next] {
-				if _,ok := available[next] ; ok {
-					continue
-				}
-				seen[next] = true
-				res = append(res,next)
-				if backtrack(res) {
-					flag = true
-				}else{
-					available[next] = false
-				}
-				res = res[:len(res)-1]
-				seen[next] = false
+			if unavailable[next] {
+				continue
 			}
+			res = append(res,next)
+			if backtrack(res) {
+				flag = true
+			}else{
+				// 从next无法到n-1
+				unavailable[next] = true
+			}
+			res = res[:len(res)-1]
 		}
 		return flag
 	}
@@ -50,24 +47,29 @@ func allPathsSourceTarget(graph [][]int) [][]int {
 	return result
 	*/
 	
+	
 	// 题目是有向无环图,则可以用递归
-	// 但最后的成绩比我用回溯差远了
 	n := len(graph)
+	// path[i]用来记录点i到n-1的所有路径，避免重复计算
+	paths := make([][][]int,n)
+	paths[n-1] = [][]int{[]int{n-1}}
 
+	// recur(v)返回所有从v到n-1的路径
 	var recur func(v int) [][]int 
 	recur = func(v int) [][]int {
 		res := [][]int{}
-		if v == n-1 {
-			res = append(res,[]int{v})
-			return res
-		}
 		for _,next := range graph[v] {
-			for _,path := range recur(next) {
-				temp := []int{v}
-				temp = append(temp,path...)
-				res = append(res,temp)
+			if len(paths[next]) != 0 {
+				for _,path := range paths[next] {
+					res = append(res,append([]int{v},path...))
+				}
+			}else{
+				for _,path := range recur(next) {
+					res = append(res,append([]int{v},path...))
+				}
 			}
 		}
+		paths[v] = res
 		return res
 	}
 
