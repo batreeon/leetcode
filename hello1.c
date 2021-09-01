5c70766e8192d578623eea4a51d4a324f80c89e0
handongyexingren <1340551309@qq.com>
2021-09-01 20:48:05 +0800

    test

--- hello1.c
+++ hello1.c
@@ -2,4 +2,20 @@
 
 int main() {
     printf("hello world!\n");
+    char s[] = "A";
+    printf("%d\n",tolower(s[0])-'a');
+    putchar(tolower(s[0]));
+    printf("\n");
+
+    int i = 0;
+    char c;
+    char str[] = "RUNOOB";
+
+    while( str[i] ) 
+    {
+        putchar(tolower(str[i]));
+        i++;
+    }
+
+    return(0);
 }
