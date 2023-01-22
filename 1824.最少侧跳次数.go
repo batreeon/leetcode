71b91c932e4cc10ee92cfc1ab29d9a747c39d6dd
batreeon <1340551309@qq.com>
2023-01-22 20:51:27 +0800

    优化了下，只用一个数组
    Change-Id: Iecd79d8aa55e05591329929c4b79130700133371

--- 1824.最少侧跳次数.go
+++ 1824.最少侧跳次数.go
@@ -11,31 +11,28 @@ func minSideJumps(obstacles []int) int {
 		return 0
 	}
 
-	starts := make([]int, 4)
-	starts[1], starts[3] = 1, 1
-	ends := make([]int, 4)
+	bp := make([]int, 4)
+	bp[1], bp[3] = 1, 1
 
 	for i := 1; i < n-1; i++ {
 		for j := 1; j < 4; j++ {
 			if j == obstacles[i] {
-				ends[j] = -1
+				bp[j] = -1
 				continue
 			}
-			if starts[j] == -1 {
-				ends[j] = -2
+			if bp[j] == -1 {
+				bp[j] = -2
 				continue
 			}
-			ends[j] = starts[j]
 		}
 		for j := 1; j < 4; j++ {
-			if ends[j] == -2 {
+			if bp[j] == -2 {
 				// 已经考虑了obstacles[i]为0的情况
-				ends[j] = mins(ends[1], ends[2], ends[3]) + 1
+				bp[j] = mins(bp[1], bp[2], bp[3]) + 1
 			}
 		}
-		copy(starts, ends)
 	}
-	return mins(starts[1], starts[2], starts[3])
+	return mins(bp[1], bp[2], bp[3])
 }
 
 func mins(x, y, z int) int {
