8564f1ea251bb56386d0aa3bfd4e4cdef6f5e37c
batreeon <1340551309@qq.com>
2023-02-03 22:31:23 +0800

    如果x是根节点，根节点将一个数分隔成两部分，只要两部分不一样大，然后y取较大子树的根就可以了，注意n是奇数，两玩家不可能分数分数相同；如果x不是根节点，那么可以将树分隔为三个部分，只要最大的一部分比剩下两部分加起来还大，那么y取较大子树和x的邻节点就可以了
    Change-Id: I166fde5cde15423bea696a0a7389c094d2a7c5a1

---
+++ 1145.二叉树着色游戏.go
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=1145 lang=golang
+ *
+ * [1145] 二叉树着色游戏
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func btreeGameWinningMove(root *TreeNode, n int, x int) bool {
+	if root.Val == x {
+		left, right := dfs(root.Left), dfs(root.Right)
+		if left != right {
+			return true
+		}
+		return false
+	}
+
+	nodes := []*TreeNode{root}
+	subleft, subright := 0, 0
+	for len(nodes) != 0 {
+		l := len(nodes)
+		for i := 0; i < l; i++ {
+			if nodes[i].Val == x {
+				subleft = dfs(nodes[i].Left)
+				subright = dfs(nodes[i].Right)
+				nodes = nil
+				break
+			}
+			if nodes[i].Left != nil {
+				nodes = append(nodes, nodes[i].Left)
+			}
+			if nodes[i].Right != nil {
+				nodes = append(nodes, nodes[i].Right)
+			}
+		}
+		if nodes != nil {
+			nodes = nodes[l:]
+		}
+	}
+
+	s, m, b := sortInts([]int{subleft, subright, n - 1 - subleft - subright})
+	if b > m+s+1 {
+		return true
+	}
+	return false
+}
+
+func dfs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + dfs(root.Left) + dfs(root.Right)
+}
+
+func sortInts(ints []int) (int,int,int) {
+	sort.Ints(ints)
+	return ints[0], ints[1], ints[2]
+}
+// @lc code=end
+
