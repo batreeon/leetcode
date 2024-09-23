ce16a1e2137c7562843dedf24d4bf1e7d852744e
batreeon <1340551309@qq.com>
2024-09-24 00:39:00 +0800

    看的答案，维护j左侧最大的 vi + i
    Change-Id: If6d856459c208814114ccd09fcd3e93dc932a504

---
+++ 1014.最佳观光组合.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1014 lang=typescript
+ *
+ * [1014] 最佳观光组合
+ */
+
+// @lc code=start
+function maxScoreSightseeingPair(values: number[]): number {
+    let maxviaddi = 0;
+    let result = 0;
+    for (let j = 0; j < values.length; j++) {
+        result = Math.max(result, maxviaddi + values[j] - j);
+        maxviaddi = Math.max(maxviaddi, values[j] + j);
+    }
+    return result;
+};
+// @lc code=end
+
