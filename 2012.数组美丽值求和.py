fdb2936c0ef9a33df0dc5fb7538c73ba348b61ee
batreeon <1340551309@qq.com>
2025-03-11 01:08:57 +0800

    不太难,前缀最大值和后缀最小值

---
+++ 2012.数组美丽值求和.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=2012 lang=python3
+#
+# [2012] 数组美丽值求和
+#
+
+# @lc code=start
+class Solution:
+    def sumOfBeauties(self, nums: List[int]) -> int:
+        premax: list[int] = [0] * len(nums)
+        premax[0] = nums[0]
+        suffmax: list[int] = [0] * len(nums)
+        suffmax[-1] = nums[-1]
+        for i in range(1, len(nums)):
+            premax[i] = max(premax[i-1], nums[i])
+        for i in range(len(nums)-2, -1, -1):
+            suffmax[i] = min(suffmax[i+1], nums[i])
+
+        result: int = 0
+        for i in range(1, len(nums)-1):
+            if premax[i-1] < nums[i] and nums[i] < suffmax[i+1]:
+                result += 2
+            elif nums[i-1] < nums[i] and nums[i] < nums[i+1]:
+                result += 1
+        
+        return result
+# @lc code=end
+
