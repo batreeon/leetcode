854dfd725d9616c426d603ec3b309eafa0a56d6a
batreeon <1340551309@qq.com>
2025-08-21 21:59:23 +0800

    注意python //是向负无穷取整，用int()向0取整

---
+++ 150.逆波兰表达式求值.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=150 lang=python3
+#
+# [150] 逆波兰表达式求值
+#
+
+# @lc code=start
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token in ['+','-','*',"/"]:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if token == '+':
+                    stack.append(num1 + num2)
+                elif token == '-':
+                    stack.append(num1 - num2)
+                elif token == '*':
+                    stack.append(num1 * num2)
+                elif token == '/':
+                    stack.append(int(num1 / num2))
+            else:
+                stack.append(int(token))
+        return stack.pop()
+# @lc code=end
+
